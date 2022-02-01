40d53df9d509c1f80d986aa724d3d2a5096edcc2
Ashsenior <ashutoshjunior@gmail.com>
2022-02-01 21:43:07 +0530

    updated few files

--- college/conditional/gross_salary.c
+++ college/conditional/gross_salary.c
@@ -5,18 +5,16 @@ void gross(float n){
     if (n<=10000){
         DA = n*0.2;
         HRA = n*0.8;
-        printf(" Your Gross salary is : %f",n+DA+HRA);
     }
     else if(n<=20000){
         DA = n*0.25;
         HRA = n*0.9;
-        printf(" Your Gross salary is : %f",n+DA+HRA);
     }
     else{
         DA = n*0.3;
         HRA = n*0.95;
-        printf(" Your Gross salary is : %f",n+DA+HRA);
     }
+    printf(" Your Gross salary is : %f",n+DA+HRA);
 }
 
 int main(){
